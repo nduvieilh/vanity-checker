abb590fadbbd4130506c20290bccb01ccc982a08
Nicky Duvieilh <nickyduvieilh@gmail.com>
2019-04-04 18:45:42 -0500

    Removing build folder from server

--- server/build/api/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-var express_1 = tslib_1.__importDefault(require("express"));
-var plates_router_controller_1 = tslib_1.__importDefault(require("./plates/plates-router.controller"));
-var router = express_1.default.Router();
-router.use('/plates', plates_router_controller_1.default);
-exports.default = router;
-//# sourceMappingURL=index.js.map
